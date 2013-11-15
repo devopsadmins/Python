cb4d519915f52efa9eb1b9e6c8d62650d2247191
Everton Lima <webdevelopersjc@yahoo.com.br>
2013-11-15 18:07:10 -0200

    Primeiro Commit de Teste

---
+++ aula1.py
@@ -0,0 +1,12 @@
+__author__ = 'everton.lima'
+str="Teste de String"
+print(str)
+print type(str)
+print (str[0:8])
+print (str[2:8])
+print (str[:9])
+print(str[::-1])
+str=raw_input("Digite o Valor:")
+print str
+str2=raw_input("Digite Valor 2:")
+print int(str)+int(str2)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Aprendendo1',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='everton.lima',
+    author_email='webdevelopersjc@yahoo.com.br',
+    description=''
+)
